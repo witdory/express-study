bdd60f5794b2da3d21c5573a3334d2c827e02535
witdory <yoonbal0406@naver.com>
2025-04-08 16:19:16 +0900

    style: 불필요한 주석 제거

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -1,14 +1,9 @@
 const checkLogin = (req, res, next) =>{
-    // if(!req.user){
-    //   return res.redirect('/login')
-    // }
+    
     if (!req.user) {
       return res.redirect('/auth/login?redirect=' + encodeURIComponent(req.originalUrl));
     }
-    // if (!req.user) {
-    //   req.session.redirectTo = req.originalUrl;
-    //   return res.redirect('/login');
-    // }
+    
     next()
   }
 
